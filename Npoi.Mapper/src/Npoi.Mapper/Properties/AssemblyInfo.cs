f400dc35633fa5d4b6d36a9e6c2f4ef0214a5a74
dtian <dtian@outlook.com>
2017-07-15 16:49:16 +0800

    fix #9 Remove `NetStandard.Library` from .csproj file

--- Npoi.Mapper/src/Npoi.Mapper/Properties/AssemblyInfo.cs
+++ Npoi.Mapper/src/Npoi.Mapper/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Npoi.Mapper")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("Npoi.Mapper")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
